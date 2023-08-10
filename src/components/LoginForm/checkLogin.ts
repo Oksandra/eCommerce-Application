dbff8b9451de91cc41b67837cbbe27f574ea45dc
KainAra <kainara.idwsi@gmail.com>
2023-08-10 15:29:29 +0300

    fix: run prettier and fix other

--- src/components/LoginForm/checkLogin.ts
+++ src/components/LoginForm/checkLogin.ts
@@ -3,7 +3,7 @@ export function checkLogin(value: string): string {
   if (value !== value.trim())
     return 'Email address must not contain leading or trailing whitespace.';
   if (value.split('@').length < 2)
-    return "Email address must contain an '@' symbol separating local part and domain name.";
+    return "Email address must contain an '@' symbol.";
   if (value.split('@').length > 2 || value.split(' ').length > 1)
     return 'Email address is not correct.';
   const domain: string = value.split('@')[1];
