efd0e9bb4eed45723addee96409994b84651211d
KainAra <kainara.idwsi@gmail.com>
2023-08-12 17:31:19 +0300

    fix: fix mistakes

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Button } from './Button';
 
-describe('LoginForm', () => {
-  test('renders login form correctly', () => {
+describe('Button', () => {
+  test('renders correctly', () => {
     render(<Button className="test" textContent="Button" type="button" />);
 
     expect(screen.getByText('Button')).toBeInTheDocument();
