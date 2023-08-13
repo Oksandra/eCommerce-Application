a939da80528c4e4031f6ba46bf38d04e6386fb06
Oksana <5oks5@mail.ru>
2023-08-13 17:57:40 +0400

    feat: add registration link to login form

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import { LayoutPage } from './containers/LayoutPage/LayoutPage';
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
+import { LoginForm } from './components/LoginForm/LoginForm';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<LayoutPage />}>
+        <Route path="login" element={<LoginForm />} />
         {/* <Route index element={<Main />} /> */}
         <Route path="*" element={<NotFoundPage />} />
       </Route>
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './_loginForm.scss';
 import { checkLogin } from '../../helpers/checkLogin';
 import { checkPassword } from '../../helpers/checkPassword';
@@ -87,6 +88,12 @@ export const LoginForm: FC = () => {
       >
         Log in
       </button>
+      <p>
+        Don❜t have an account?
+        <Link className="login-form__link" to="/registration">
+          Sign UP
+        </Link>
+      </p>
     </form>
   );
 };
