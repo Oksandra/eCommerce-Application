e0409066aaa73e29f7d0757c768f9de5d0f3d265
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-08-14 15:37:37 +0300

    fix: correct app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.scss';
-/* import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { LayoutPage } from './containers/LayoutPage/LayoutPage';
-import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage'; */
-import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
 
 function App(): JSX.Element {
-  return <RegistrationForm />;
-  /* <Routes>
-<Route path="/" element={<LayoutPage />}> */
-  /* {/* <Route index element={<Main />} /> */
-  /* <Route path="*" element={<NotFoundPage />} />
+  return (
+    <Routes>
+      <Route path="/" element={<LayoutPage />}>
+        {/* <Route index element={<Main />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
-  ); */
+  );
 }
 
 export default App;
