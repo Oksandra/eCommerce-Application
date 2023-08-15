15c860e55558aa95f8d64f66591b20a684d83211
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-08-15 14:22:18 +0300

    fix: correct checkPostalCode function

--- src/helpers/checkPostalCode.tsx
+++ src/helpers/checkPostalCode.tsx
@@ -2,7 +2,13 @@ import { postcodeValidator } from 'postcode-validator';
 import { countries } from '../components/SelectCountries/countries';
 
 export default function checkPostalCode(data: string): boolean {
-  const country = document.querySelector('option.selected')?.textContent;
+  const countryElement = document.querySelector('option.selected');
+  let country: string;
+  if (countryElement instanceof HTMLElement) {
+    country = countryElement?.textContent as string;
+  } else {
+    country = 'Andora';
+  }
   const code = countries.find((el) => el.country === country)?.code as string;
   return postcodeValidator(data, code);
 }
