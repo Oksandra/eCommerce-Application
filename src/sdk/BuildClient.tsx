ee59316693708be3dcff575b1934a5f6b13be8ba
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-08-17 17:06:49 +0300

    feat: add sdk files for requests

---
+++ src/sdk/BuildClient.tsx
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import {
+  ClientBuilder,
+
+  // Import middlewares
+  type AuthMiddlewareOptions, // Required for auth
+  type HttpMiddlewareOptions, // Required for sending HTTP requests
+} from '@commercetools/sdk-client-v2';
+
+const oauthUri = 'https://auth.europe-west1.gcp.commercetools.com';
+const baseUri = 'https://api.europe-west1.gcp.commercetools.com';
+const credentials = {
+  clientId: 'U_dKA1aIeI2QrDLPSYglNmQ4',
+  clientSecret: 'ddPY7bXFPhdMw3v_v06R_pIqDmooV-Ia',
+};
+const projectKeyApi = 'ecommerce-application3';
+const scopes = ['manage_project:ecommerce-application3'];
+
+// Configure authMiddlewareOptions
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: oauthUri,
+  projectKey: projectKeyApi,
+  credentials: {
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+  },
+  scopes,
+  fetch,
+};
+
+// Configure httpMiddlewareOptions
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: baseUri,
+  fetch,
+};
+
+// Export the ClientBuilder
+const ctpClient = new ClientBuilder() // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withLoggerMiddleware() // Include middleware for logging
+  .build();
+
+export { ctpClient, projectKeyApi };
