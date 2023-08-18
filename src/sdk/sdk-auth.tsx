5deecff351b5174c65b7d75141ace6c05a763740
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-08-19 01:01:52 +0300

    feat: add sdk-auth

--- src/sdk/sdk-auth.tsx
+++ src/sdk/sdk-auth.tsx
@@ -9,22 +9,22 @@ interface DisableToken {
 }
 
 const authClient = new SdkAuth({
-  host: 'https://auth.commercetools.com',
+  host: 'https://auth.europe-west1.gcp.commercetools.com',
   projectKey: projectKeyApi,
   disableRefreshToken: false,
   credentials: {
     clientId: 'U_dKA1aIeI2QrDLPSYglNmQ4',
     clientSecret: 'ddPY7bXFPhdMw3v_v06R_pIqDmooV-Ia',
   },
-  scopes: ['view_products:test', 'manage_orders:test'],
+  scopes: ['manage_project:ecommerce-application3'],
   fetch,
 });
 
-const loginCustomer = async (
+const getCustomerToken = async (
   user: UserLogin,
   disableToken: DisableToken
 ): Promise<TokenInfo> => {
   return authClient.customerPasswordFlow(user, disableToken);
 };
 
-export default loginCustomer;
+export default getCustomerToken;
