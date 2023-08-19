b280dca5350f2549ce9398a94f1e0d18e107a8e6
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-08-19 19:39:11 +0300

    fix: correct message of error of password

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -134,7 +134,7 @@ function RegistrationForm(): JSX.Element {
             {errors.password && (
               <p>
                 {errors.password.message ||
-                  'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number!'}
+                  'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character!'}
               </p>
             )}
           </div>
