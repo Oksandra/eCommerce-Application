621db58e0472369e7576b90761ecad180cc04f8e
KainAra <kainara.idwsi@gmail.com>
2023-08-20 13:22:53 +0300

    feat: add burger menu

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Nav.scss';
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import { HeaderContext } from '../../containers/Header/HeaderContext';
 
 export const Nav: React.FC = () => {
   const { user } = useAuth();
   const logLink = user ? '/logout' : '/login';
+  const headerContext = useContext(HeaderContext) === 'true';
 
   return (
-    <nav className="nav">
+    <nav className={`nav ${headerContext ? 'nav_active' : ''}`}>
       <ul className="nav__list">
         <li className="nav__item">
           <NavLink className="nav__link" to="/">
