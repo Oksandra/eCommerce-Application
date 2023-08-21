d4a75059272214f7c9e543b7aec1ff1f7cbf6595
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-08-21 19:01:33 +0300

    fix: add condition for errors in billing address

--- src/components/Addresses/Adressess.tsx
+++ src/components/Addresses/Adressess.tsx
@@ -244,7 +244,7 @@ const Addresses: React.FC<AddressesProps> = ({
             disabled={isBillingAddressSame}
           />
           <div className="input-error">
-            {errors.billing?.street && (
+            {errors.billing?.street && !isBillingAddressSame && (
               <p id="billing-street-error">
                 Billing street is required and must contain at least one
                 character!
@@ -269,7 +269,7 @@ const Addresses: React.FC<AddressesProps> = ({
             disabled={isBillingAddressSame}
           />
           <div className="input-error">
-            {errors.billing?.city && (
+            {errors.billing?.city && !isBillingAddressSame && (
               <p id="billing-city-error">
                 {errors.billing.city.message ||
                   'Billing city must not contain special characters or numbers!'}
@@ -318,7 +318,7 @@ const Addresses: React.FC<AddressesProps> = ({
             disabled={isBillingAddressSame}
           />{' '}
           <div className="input-error">
-            {errors.billing?.postcode && (
+            {errors.billing?.postcode && !isBillingAddressSame && (
               <p id="billing-code-error">
                 {errors.billing.postcode.message || 'Incorrect postal code!'}
               </p>
