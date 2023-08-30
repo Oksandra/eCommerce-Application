ebbba3618d5d1b820919f81addebbea60d28a360
Oksana <5oks5@mail.ru>
2023-08-31 01:27:09 +0400

    feat: add images to page

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,27 +1,72 @@
 import React, { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ProductData } from '@commercetools/platform-sdk';
 import getProduct from '../../api/getProduct';
 import './ProductPage.scss';
 import Loader from '../../components/Loader/Loader';
 
+interface Image {
+  url: string;
+  dimensions: {
+    w: number;
+    h: number;
+  };
+}
+
 const ProductPage: React.FC = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [isLoading, setIsloading] = useState<boolean>(true);
   const [product, setProduct] = useState<ProductData | null>(null);
+  const [images, setImages] = useState<Image[] | undefined>();
   React.useEffect(() => {
     getProduct(`${id}`)
       .then((data) => {
         setProduct(data.body.masterData.current);
+        console.log(data.body.masterData.current);
+        setImages(data.body.masterData.current.masterVariant.images);
         setIsloading(false);
       })
-      .catch(() => navigate('/*'));
+      .catch((e) => console.log(e));
   }, [id]);
 
   return (
-    <div className="product-card">
-      {isLoading ? <Loader /> : product && product.name['en-US']}
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="product-card">
+          <div className="product-card__wrapper">
+            <div className="images">
+              <div className="images__main">
+                <img
+                  src={images ? images[0].url : ''}
+                  className="product-card__img"
+                  alt="bottle of wine"
+                />
+              </div>
+              <div className="images__additional">
+                <img
+                  src={images ? images[0].url : ''}
+                  className="images__card"
+                  alt="a lot of wine"
+                />
+                <img
+                  src={images ? images[1].url : ''}
+                  className="images__card"
+                  alt="a lot of wine"
+                />
+                <img
+                  src={images ? images[2].url : ''}
+                  className="images__card"
+                  alt="a lot of wine"
+                />
+              </div>
+            </div>
+            <p>{product && product.name['en-US']}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
