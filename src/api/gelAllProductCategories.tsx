a988258e302fab64c6909bdb102bf34b90523840
Oksana <5oks5@mail.ru>
2023-09-01 00:13:38 +0400

    feat: add request for get all categories

---
+++ src/api/gelAllProductCategories.tsx
@@ -0,0 +1,22 @@
+import {
+  ClientResponse,
+  ProductTypePagedQueryResponse,
+} from '@commercetools/platform-sdk';
+import apiRoot from '../sdk/client';
+
+const getTypesProducts = (): Promise<
+  ClientResponse<ProductTypePagedQueryResponse>
+> => {
+  return apiRoot
+    .productTypes()
+    .get({
+      queryArgs: {
+        limit: 10,
+        offset: 1,
+        withTotal: true,
+      },
+    })
+    .execute();
+};
+
+export default getTypesProducts;
