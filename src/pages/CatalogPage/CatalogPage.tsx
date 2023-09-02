1d594b45d9e2c073c1fdc27d3b8b8777e42e3c44
Oksana <5oks5@mail.ru>
2023-09-02 17:37:58 +0400

    feat: add loader to catalog page

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -4,39 +4,49 @@ import './CatalogPage.scss';
 import { Product } from '@commercetools/platform-sdk';
 import getAllProducts from '../../api/getAllProducts';
 import { ProductCard } from '../../components/ProductCard/ProductCard';
+import Loader from '../../components/Loader/Loader';
 
 const CatalogPage: React.FC = () => {
   const [allProducts, setAllProducts] = useState<Product[]>([]);
   const navigate = useNavigate();
+  const [isLoading, setIsloading] = useState<boolean>(true);
+
   React.useEffect(() => {
-    getAllProducts(1)
+    getAllProducts(0)
       .then((data) => {
         setAllProducts(data.body.results);
+        setIsloading(false);
       })
       .catch((e) => console.log(e));
   }, []);
 
   return (
     <div className="catalog">
-      <div className="catalog__wrapper">
-        {allProducts.map((product) => (
-          <ProductCard
-            title={product.masterData.current.name['en-US']}
-            image={
-              product.masterData.current.masterVariant.images
-                ? product.masterData.current.masterVariant.images[0].url
-                : ''
-            }
-            desc={
-              product.masterData.current.metaDescription
-                ? product.masterData.current.metaDescription['en-US']
-                : ''
-            }
-            price="100"
-            key={product.id}
-            onClick={(): void => navigate(`/catalog/${product.id}`)}
-          />
-        ))}
+      <div
+        className={isLoading ? 'catalog__wrapper empty' : 'catalog__wrapper'}
+      >
+        {isLoading ? (
+          <Loader />
+        ) : (
+          allProducts.map((product) => (
+            <ProductCard
+              title={product.masterData.current.name['en-US']}
+              image={
+                product.masterData.current.masterVariant.images
+                  ? product.masterData.current.masterVariant.images[0].url
+                  : ''
+              }
+              desc={
+                product.masterData.current.metaDescription
+                  ? product.masterData.current.metaDescription['en-US']
+                  : ''
+              }
+              price="100"
+              key={product.id}
+              onClick={(): void => navigate(`/catalog/${product.id}`)}
+            />
+          ))
+        )}
       </div>
     </div>
   );
