34361133236844ae8f656cf9d7512d9ee0982ed5
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-09-06 01:58:29 +0300

    feat: add requests for get products by category

---
+++ src/api/getProductsByCategory.tsx
@@ -0,0 +1,23 @@
+import {
+  ClientResponse,
+  ProductProjectionPagedQueryResponse,
+} from '@commercetools/platform-sdk';
+import apiRoot from '../sdk/client';
+
+const getProductsByCategory = (
+  idCategory: string
+): Promise<ClientResponse<ProductProjectionPagedQueryResponse>> => {
+  return apiRoot
+    .productProjections()
+    .get({
+      queryArgs: {
+        where: `productType(id="${idCategory}")`,
+        limit: 16,
+        offset: 1,
+        withTotal: true,
+      },
+    })
+    .execute();
+};
+
+export default getProductsByCategory;
