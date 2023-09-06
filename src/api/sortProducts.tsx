f8544b4061f888353a2b5c45cf480dc272792fe9
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-09-06 10:08:46 +0300

    feat: add sorting by name

---
+++ src/api/sortProducts.tsx
@@ -0,0 +1,24 @@
+import {
+  ClientResponse,
+  ProductProjectionPagedSearchResponse,
+} from '@commercetools/platform-sdk';
+import apiRoot from '../sdk/client';
+
+// В качесте type должна приходить строка asc - для сортировки от A-Z, desc - от Z-A
+
+const sortProductsByName = (
+  type: string
+): Promise<ClientResponse<ProductProjectionPagedSearchResponse>> => {
+  return apiRoot
+    .productProjections()
+    .search()
+    .get({
+      queryArgs: {
+        limit: 100,
+        sort: [`name.en-us ${type}`],
+      },
+    })
+    .execute();
+};
+
+export default sortProductsByName;
