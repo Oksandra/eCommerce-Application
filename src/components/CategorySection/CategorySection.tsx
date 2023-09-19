a9f5b5294ee2c5d81775b3c86fec36f6d1e5274a
Oksana <5oks5@mail.ru>
2023-09-19 20:54:07 +0400

    feat: add captions to categories

--- src/components/CategorySection/CategorySection.tsx
+++ src/components/CategorySection/CategorySection.tsx
@@ -7,10 +7,18 @@ const CategorySection: React.FC = () => {
       <div className="category__wrapper">
         <h3 className="category__title">Shop by category</h3>
         <div className="category__items">
-          <div className="category__item category__item_red" />
-          <div className="category__item category__item_white" />
-          <div className="category__item category__item_rose" />
-          <div className="category__item category__item_sparkling" />
+          <div className="category__item category__item_red">
+            <p className="category__text">RED WINE</p>
+          </div>
+          <div className="category__item category__item_white">
+            <p className="category__text">WHITE WINE</p>
+          </div>
+          <div className="category__item category__item_rose">
+            <p className="category__text">ROSE WINE</p>
+          </div>
+          <div className="category__item category__item_sparkling">
+            <p className="category__text">SPARKLING WINE</p>
+          </div>
         </div>
       </div>
     </section>
